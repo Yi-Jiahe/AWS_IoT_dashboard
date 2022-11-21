81480a3d8726261351afc047d92cb0363fa61bd1
Yi Jiahe <yi.jiahe00@gmail.com>
2022-11-21 17:40:42 +0800

    setup environmental data get endpoint w/ dummy data

--- server/iotmanager/data/views.py
+++ server/iotmanager/data/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view(['GET'])
 def environmental_data(request):
-    return HttpResponse("Hello, world. Here's some environmental data.")
+    return Response([{
+        'timestamp': 1234,
+        'temperature': 24.0,
+        'humidity': 50.0,
+    }])
